60180e219e5fcd2180a2f27064b346d67ac50b84
Deepender Singla <deepender281190@gmail.com>
2013-04-18 03:08:07 -0700

    Update check.rb
    comments added

--- check.rb
+++ check.rb
@@ -9,8 +9,13 @@ agent.page.forms[0]["lccp_pnrno1"] = Pnr_no
 button = agent.page.forms[0].button_with(:value => "Get Status")
 # submit the form using the button
 pages=agent.submit(form, button)
+#train number query
 puts pages.at('table.table_border').search('.//tr[3]/td[1]').text
+#Journey date
 puts pages.at('table.table_border').search('.//tr[3]/td[3]').text
+#starting station
 puts pages.at('table.table_border').search('.//tr[3]/td[4]').text
+#end Station
 puts pages.at('table.table_border').search('.//tr[3]/td[5]').text
+#current status -seat no
 puts pages.at('.//table[@id="center_table"]/tr[2]/td[2]').text
